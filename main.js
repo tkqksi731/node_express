d6a99da0ebba4587b6d0a8349dac1ef7a0f497a9
tkqksi731 <50672888+tkqksi731@users.noreply.github.com>
2021-01-13 15:35:12 +0900

    페이지 수정 구현 - page update

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const template = require('./lib/template.js');
 const sanitizeHtml = require('sanitize-html');
+const qs = require('querystring');
 const express = require('express')
 const app = express()
 const port = 3000
@@ -27,6 +28,7 @@ app.get('/', function(request, response) {
     });
 });
 
+// 상세 페이지 구현
 // Route parameters - express
 app.get('/page/:pageId', function(request, response) {
   // key : value 방식
@@ -40,7 +42,7 @@ app.get('/page/:pageId', function(request, response) {
       let html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescroption}`,
         `<a href="/create">create</a>
-        <a href="/update?id=${sanitizedTitle}">update</a>
+        <a href="/update/${sanitizedTitle}">update</a>
         <form action="delete_process" method="post">
           <input type="hidden" name="id" value="${sanitizedTitle}">
           <input type="submit" value="delete">
@@ -49,6 +51,104 @@ app.get('/page/:pageId', function(request, response) {
         response.send(html);
     });
   });
+});
+
+// 페이지 생성
+app.get('/create', function(request, response){
+  fs.readdir('./data', function(error, filelist){
+      let title = 'WEB - create';
+      let list = template.list(filelist);
+      let html = template.HTML(title, list, `
+      <form action="/create_process" method="post">
+      <p>
+        <input type="text" name="title" placeholder="title">
+      </p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+  `, '');
+    // create 부분 만들기(url, 요청)
+      response.send(html);
+    });
+});
+
+// 페이지 생성 POST 방식
+app.post('/create_process', function(request, response){
+  let body = '';
+    request.on('data', function(data){
+        // 웹 브라우저가 POST방식으로 전송할 떄 data의 양이 많으면 함수 호출하도록 약속
+      body = body + data;
+    });
+    request.on('end', function(){
+      // 들어올 정보가 더 이상 없으면 정보 수신 끝
+      let post = qs.parse(body);
+      let title = post.title;
+      let description = post.description;
+      // 제목과 내용 업로드
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err) { // err가 있을 경우 처리 방식
+        response.writeHead(302, {Location: `/page/${title}`}); // 보내고 싶은 위치의 주소
+        response.end('success');
+      });
+    });
+});
+
+// 페이지 업데이트
+app.get('/update/:pageId', function(request, response){
+  fs.readdir('./data', function(error, filelist){
+      const filteredId = path.parse(request.params.pageId).base; // security
+      fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+        let title = request.params.pageId;
+        let list = template.list(filelist);
+        let html = template.HTML(title, list,
+          //hidden으로 하여 id 값을 받고 변경된 title을 삽입
+          `
+          <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p>
+              <input type="text" name="title" placeholder="title" value="${title}">
+            </p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          );
+          // 업데이트 시 선택한 제목과 내용에 대하여 불러와 title, description text 박스에 보이기
+          response.send(html);
+      });
+    });
+});
+
+app.post('/update_process', function(request, response){
+        let body = '';
+      request.on('data', function(data){
+          // 웹 브라우저가 POST방식으로 전송할 떄 data의 양이 많으면 함수 호출하도록 약속
+        body = body + data;
+      });
+      request.on('end', function(){
+        // 들어올 정보가 더 이상 없으면 정보 수신 끝
+        let post = qs.parse(body);
+        let id = post.id;
+        let title = post.title;
+        let description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error){
+          // 제목과 내용 수정 업로드
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err) { // err가 있을 경우 처리 방식
+            response.writeHead(302, {Location: `/page/${title}`}); // 보내고 싶은 위치의 주소
+            response.end();
+          })
+        })
+        // console.log(post);
+      
+      });
 });
 
 app.listen(port, () => {
@@ -74,72 +174,10 @@ app.listen(port, () => {
 //     // console.log(pathname);
 //     if(pathname === '/'){
 //       if(queryData.id === undefined){
-//       } else {
-//         fs.readdir('./data', function(error, filelist){
-//           const filteredId = path.parse(queryData.id).base; // security
-//           fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-//             let title = queryData.id;
-//             let sanitizedTitle = sanitizeHtml(title);
-//             let sanitizedDescroption = sanitizeHtml(description);
-//             let list = template.list(filelist);
-//             let html = template.HTML(sanitizedTitle, list,
-//               `<h2>${sanitizedTitle}</h2>${sanitizedDescroption}`,
-//               `<a href="/create">create</a>
-//               <a href="/update?id=${sanitizedTitle}">update</a>
-//               <form action="delete_process" method="post">
-//                 <input type="hidden" name="id" value="${sanitizedTitle}">
-//                 <input type="submit" value="delete">
-//               </form>`
-//               );
-//             response.writeHead(200);
-//             response.end(html);
-//           });
-//         });
+//       } 
 //       }
 //     } else if(pathname === '/create'){
-
-//         fs.readdir('./data', function(error, filelist){
-            
-//             let title = 'WEB - create';
-//             let list = template.list(filelist);
-//             let html = template.HTML(title, list, `
-//             <form action="/create_process" method="post">
-//             <p>
-//               <input type="text" name="title" placeholder="title">
-//             </p>
-//             <p>
-//               <textarea name="description" placeholder="description"></textarea>
-//             </p>
-//             <p>
-//               <input type="submit">
-//             </p>
-//           </form>
-//         `, '');
-//           // create 부분 만들기(url, 요청)
-//             response.writeHead(200);
-//             response.end(html);
-//           });
-    
 //     } else if(pathname === '/create_process') {
-//       let body = '';
-//       request.on('data', function(data){
-//           // 웹 브라우저가 POST방식으로 전송할 떄 data의 양이 많으면 함수 호출하도록 약속
-//         body = body + data;
-//       });
-//       request.on('end', function(){
-//         // 들어올 정보가 더 이상 없으면 정보 수신 끝
-//         let post = qs.parse(body);
-//         let title = post.title;
-//         let description = post.description;
-//         // 제목과 내용 업로드
-//         fs.writeFile(`data/${title}`, description, 'utf8', function(err) { // err가 있을 경우 처리 방식
-//           response.writeHead(302, {Location: `/?id=${title}`}); // 보내고 싶은 위치의 주소
-//           response.end('success');
-//         });
-//         // console.log(post.title);
-//       });
-      
-
 //     } else if(pathname === '/update'){
 //       fs.readdir('./data', function(error, filelist){
 //         const filteredId = path.parse(queryData.id).base; // security
